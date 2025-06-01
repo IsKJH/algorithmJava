3b037e3f011d0c943dcf15bbc4c2a373fd58dd09
IsKJH <dkdlrh147@gmail.com>
2025-06-02 08:58:34 +0900

    학습

--- src/baek/Baek_2908.java
+++ src/baek/Baek_2908.java
@@ -5,13 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Baek_2908 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] str = br.readLine().split(" ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
 
-		for (int i = 0; i < 3; i++) {
-			String temp = String.valueOf(str[0].charAt(i));
-			String temp1 = String.valueOf(str[1].charAt(i));
-		}
-	}
+        String temp1 = str[0];
+        String temp2 = str[1];
+
+        String temp_1 = String.valueOf(temp1.charAt(2)) + String.valueOf(temp1.charAt(1)) + String.valueOf(temp1.charAt(0));
+        String temp_2 = String.valueOf(temp2.charAt(2)) + String.valueOf(temp2.charAt(1)) + String.valueOf(temp2.charAt(0));
+        System.out.println(Math.max(Integer.parseInt(temp_1), Integer.parseInt(temp_2)));
+    }
 }
